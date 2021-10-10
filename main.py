b3c32a1bb1ef65ed4dc6bc04add1b804fb5a5125
Жилевич Анастасия <89995065+AnaZni@users.noreply.github.com>
2021-10-10 18:20:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,24 @@ for i in range(0, len(lis)-1, 2):
     lis[i], lis[i+1] = lis[i+1], lis[i]
 
 print(lis)
+
+
+***************
+number =int(input("Введите номер месяца: \n> "))
+months={1:'Январь',2:'Февраль',3:'Март',4:'Апрель',5:'Май',6:'Июнь',7:'Июль',8:'Август',9:'Сентябрь',10:'Октябрь',11:'Ноябрь',12:'Декабрь'}
+if number<13:
+    print(months[number])
+else:print("Такого месяца нет")
+    
+    
+**************
+while True:
+    str = input('ВВедите слова: ')
+    if len(str) < 3 or str.count(' ') < 1:
+        print('Попробуйте еще раз')
+        continue
+
+    break
+
+for index, word in enumerate(str.split()):
+    print(index + 1, (word, word[:10])[len(word) > 10])
